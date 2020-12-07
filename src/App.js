b5406178f1d7b89f525d3bcdbbb83f83d87d72a0
max245 <48263083+max245@users.noreply.github.com>
2020-12-06 17:41:23 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+           <code>Sekube-6ix Monitoring Tool under Development</code> 
+        </p>
+        <a
+        
+        >
+       
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
